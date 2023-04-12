0996fd2bec94e9e9cf407e8d480b10bd4a3af8a6
ralren <ren@truss.works>
2023-04-12 15:29:21 -0700

    try to decode again

--- app/handler.py
+++ app/handler.py
@@ -1,9 +1,9 @@
 from slack import WebClient
+from base64 import b64decode
 
 import logging
 import time
 import json
-import base64
 
 logger = logging.getLogger() 
 logger_level = logging.INFO  
@@ -35,8 +35,8 @@ def oauth_URI(scope, client_id, redirect_uri):
 def handler(event, context):
       start_t = time.time()
       log("Received event", dict(event=event))
-      body = base64.b64decode(event.get('body'))
-      log("Decoded body", dict(body=body))
+      slack_body = b64decode(event.get('body'))
+      log("Decoded body", dict(body=slack_body))
 
       # parse the event and see which slash command was called
       # redirect to the correct method in bot.py
