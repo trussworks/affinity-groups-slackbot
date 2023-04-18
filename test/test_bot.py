20aa7c1f6699d93b34501fd9034c0d13b53f8b2d
ralren <ren@truss.works>
2023-04-18 10:02:37 -0700

    fix pytest

--- test/test_bot.py
+++ test/test_bot.py
@@ -1,17 +1,16 @@
 from unittest import TestCase, mock
 
-from handler import query_team_id
-
 import os
 
 
 class QueryTeamIdTests(TestCase):
-    @mock.patch("slack.WebClient")
     @mock.patch.dict(os.environ, {"SLACK_BOT_USER_TOKEN": "beep user token"})
+    @mock.patch("slack.WebClient")
     def test_calls_slack_client_api_method(self, SlackClient):
         mock_api_response = {"ok": True, "team": {"id": "T12345"}}
         SlackClient.return_value.api_call.return_value = mock_api_response
 
+        from handler import query_team_id
         team_id = query_team_id()
 
         SlackClient.return_value.api_call.assert_called_once()
